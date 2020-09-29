e45232aa104189f55cc3f9e928199e489c66e4c5
James Woolfenden <James.Woolfenden@gmail.com>
2020-09-29 09:29:08 +0100

    cruft

--- example/exampleA/outputs.tf
+++
@@ -1,7 +0,0 @@
-output gateway_ipv4 {
-  value = module.network.gateway_ipv4
-}
-
-output network_self_link {
-  value = module.network.network_self_link
-}
--- example/exampleA/provider.gcp.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = var.project
-  region  = var.region
-  version = "~>3.5"
-}
