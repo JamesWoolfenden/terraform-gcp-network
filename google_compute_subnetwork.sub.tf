dd75ad9d0f20bd0c24e8d6b9cb5404a3da0794d4
James Woolfenden <james.woolfenden@gmail.com>
2022-09-04 14:15:22 +0100

    pike

--- google_compute_subnetwork.sub.tf
+++ google_compute_subnetwork.sub.tf
@@ -1,4 +1,5 @@
 resource "google_compute_subnetwork" "sub" {
+  #checkov:skip=CKV_GCP_76
   name               = local.subnet_name
   ip_cidr_range      = var.ip_cidr_range
   region             = var.region
